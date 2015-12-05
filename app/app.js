43f32d2588adf1fbbec6c083ff9b467db85ded2d
Bogdan <bogdanbrkic@Bogdans-MacBook-Pro.local>
2015-12-05 12:57:07 +0100

    node multer

--- app/app.js
+++ app/app.js
@@ -4,11 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multer = require('multer');
 
 var routes = require('./routes/index');
 var about = require('./routes/about');
 var imgmanip = require('./routes/imgmanip');
 var users = require('./routes/users');
+var uploads = require('./routes/uploads');
 
 var app = express();
 
@@ -24,12 +26,15 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+// file uploads
+app.use(multer({ dest: './uploads/'}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/about', about);
 app.use('/users', users);
+app.use('/uploads', uploads);
 app.use('/imgmanip', imgmanip);
 
 // catch 404 and forward to error handler
