cc276372808a5322b919f746d2f63bf50fafb74b
Bogdan <bogdanbrkic@Bogdans-MacBook-Pro.local>
2015-12-12 23:10:52 +0100

    clear from AW

--- app/routes/uploads.js
+++ app/routes/uploads.js
@@ -5,8 +5,11 @@ var fs = require("fs");
 
 router.get('/', function(req, res) {
   res.render("uploadPage", {title: "Upload image file:"});
+
 });
 
+
+// @TODO move to imgmanip and fetch date there and render .. 
 router.post("/upload", function(req, res, next){
   console.log('upload file ......');
   // console.log(req.body) // form fields
@@ -15,6 +18,15 @@ router.post("/upload", function(req, res, next){
     console.log(req.file);
     fs.rename(req.file.path, req.file.destination + req.file.originalname);
   }
+
+  // on upload send data / param
+  var img_name = req.file.originalname;
+  res.send(img_name);
+
+// imgmanip
+// res.render( "uploadPage", {filename: req.param('img_name')});
+
+
 });
 
 module.exports = router;
